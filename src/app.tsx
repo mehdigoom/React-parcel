d59af84e2c63af222033e87153b9ec60fe33a1a6
mehdigoom <medhi.gay@sfr.fr>
2019-11-08 14:37:32 +0100

    Routeur/Login

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,47 @@
-import * as React from "react"
+import * as React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
 
-import Login from "./js/pages/login"
+    useHistory,
 
+  } from "react-router-dom";
+import Login from "./js/pages/login";
+import Home from "./js/pages/Home";
 class App extends React.Component {
-    render() {
-        return (
-            <Login title="Login page" />
-        )
-    }
+  componentDidMount() {
+    let userFromStorage = window.localStorage.getItem("user");
+    console.log(userFromStorage);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">login</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login">
+              <Login title="my login page" isLoaded={false} />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 // L'export par défault de notre fichier app.tsx est la class App qui a été extends par React
--- src/js/pages/login.tsx
+++ src/js/pages/login.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import { Route, Redirect } from "react-router-dom";
+
 //Typescript
 interface Props {
   title: string;
@@ -9,6 +11,8 @@ interface Props {
 interface State {
   email: string;
   password: string;
+  user: object;
+  userIsDefined: boolean;
 }
 
 class Login extends React.Component<Props, State> {
@@ -17,29 +21,49 @@ class Login extends React.Component<Props, State> {
 
     this.state = {
       email: "email@test.com",
-      password: "derpderp"
+      password: "derpderp",
+      user: {},
+      userIsDefined: false
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    // window.localStorage.clear();
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state);
 
     fetch("https://jsonplaceholder.typicode.com/users", {
       method: "GET",
       headers: {
-        'Content-Type': 'application/json'
-      },
+        "Content-Type": "application/json"
+      }
     }).then(data => {
       data.json().then(results => {
-          console.log(results);
-      })
+        this.setState({
+          user: results[0],
+          userIsDefined: true
+        });
+
+        window.localStorage.setItem("user", results[0]);
+      });
     });
   }
 
   render() {
+    if (this.state.userIsDefined) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/"
+          }}
+        />
+      );
+    }
+
     return (
       <React.Fragment>
         <h1>Je suis un titre</h1>
